0bc2f8b3c68f343050d7c9669a6a49d54c8ec136
LuisEderLeon <luiseder.leonvesga@gmail.com>
2024-09-24 20:33:51 -0500

    feat :rocket: added many to many Observation table

--- ddl.sql
+++ ddl.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS `celestialBodies` (
   `id` int NOT NULL,
+  `name` varchar(50) DEFAULT NULL UNIQUE,
   `distance` double DEFAULT NULL,
   `distanceUnits` enum('lightYears','millionLightYears') DEFAULT 'lightYears',
   `diameter` double DEFAULT NULL,
@@ -10,13 +11,11 @@ CREATE TABLE IF NOT EXISTS `celestialBodies` (
 
 CREATE TABLE IF NOT EXISTS `planets` (
   `id` int NOT NULL,
-  `name` varchar(50) DEFAULT NULL,
   `mass` double DEFAULT NULL,
   `moonsAmount` int DEFAULT NULL,
   'orbitId' int NOT NULL,
   'planetType' enum('dwarf','terrestrial','gas','ice','terrestrialGiant','gasGiant','iceGiant'),
   PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`),
   FOREIGN KEY (`id`) REFERENCES `celestialBodies` (`id`),
   FOREIGN KEY (`orbitId`) REFERENCES `celestialBodies` (`id`)
 ) 
@@ -25,46 +24,39 @@ CREATE TABLE IF NOT EXISTS `telescopes` (
   `id` int NOT NULL,
   `location` varchar(50) CHARACTER SET utf32 COLLATE utf32_spanish_ci NOT NULL,
   `resolution` int NOT NULL,
-  PRIMARY KEY (`idTelescopes`),
-  UNIQUE KEY `idTelescopes_UNIQUE` (`idTelescopes`),
-  UNIQUE KEY `location_UNIQUE` (`location`)
+  PRIMARY KEY (`id`),
 )
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int NOT NULL,
+  `id` int NOT NULL UNIQUE,
   `name` varchar(50) CHARACTER SET utf32 COLLATE utf32_spanish_ci NOT NULL,
   `email` varchar(50) CHARACTER SET utf32 COLLATE utf32_spanish_ci NOT NULL,
   `phone` int NOT NULL,
   `birthDate` date NOT NULL,
   `address` varchar(50) CHARACTER SET utf32 COLLATE utf32_spanish_ci DEFAULT NULL,
-  PRIMARY KEY (`idUser`),
-  UNIQUE KEY `id` (`idUser`),
+  PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `phone` (`phone`)
 )
 
 CREATE TABLE IF NOT EXISTS `stars` (
   `id` int NOT NULL,
-  `name` varchar(50) DEFAULT NULL,
   `mass` double DEFAULT NULL,
   `planets` int DEFAULT 0,
   'orbitId' int DEFAULT NULL,
   'starType' enum('o-type','b-type','a-type','f-type','g-type','k-type','m-type','whiteDwarf'),
   PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`),
   FOREIGN KEY (`id`) REFERENCES `celestialBodies` (`id`)
   FOREIGN KEY (`orbitId`) REFERENCES `celestialBodies ` (`id`)
 )
 
 CREATE TABLE IF NOT EXISTS `blackHole` (
   `id` int NOT NULL,
-  `name` varchar(50) DEFAULT NULL,
   `mass` double DEFAULT NULL,
   `spin` double DEFAULT NULL,
   'charge' double DEFAULT NULL
   'galaxy' int DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`),
   FOREIGN KEY (`id`) REFERENCES `celestialBodies` (`id`)
   FOREIGN KEY (`galaxy`) REFERENCES `galaxy ` (`id`)
 ) 
@@ -87,12 +79,25 @@ CREATE TABLE if not exists galaxy (
 
 CREATE TABLE if not exists typeAstronomicalEvents (
 	id INT primary key auto_increment,
-    NameEvent varchar(50)
+  NameEvent varchar(50)
 );
 
 CREATE TABLE if not exists astronomicalEvents (
 	id INT primary key auto_increment,
     eventId int NOT NULL,
     eventDate date not null,
-    FOREIGN KEY (eventId) REFERENCES typeAstronomicalEvents (eventId)
+    FOREIGN KEY (eventId) REFERENCES typeAstronomicalEvents (id)
+);
+
+CREATE TABLE IF NOT EXISTS observation (
+  id INT NOT NULL UNIQUE,
+  idUser INT,
+  idTelescope INT,
+  idBody INT,
+  observationDate DATETIME,
+  timeSpent TIME,
+  FOREIGN KEY (`idUser`) REFERENCES `users` (`id`),
+  FOREIGN KEY (`idTelescope`) REFERENCES `telescopes` (`id`), 
+  FOREIGN KEY (`idBody`) REFERENCES `celestialBodies` (`id`),
+  PRIMARY KEY ('id')
 );
